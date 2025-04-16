88aeb92993551dd60d839f81ae1ce803afabc73f
hejincai <hejincai0895@gmail.com>
2025-04-16 17:26:29 +0800

    完善斐波那契数列的文档

--- chapter01/fibonacci.js
+++ chapter01/fibonacci.js
@@ -12,6 +12,26 @@ function fib_iter(n) {
     return counter === 0 ? a : fib_iteration(b, a + b, counter - 1);
   }
 }
+// 利用快速幂求解斐波那契数列
+function fast_fib(n) {
+    // a = 
+  return fast_fib_iter(1, 0, 0, 1, n);
+  function fast_fib_iter(a, b, p, q, n) {
+    if (n == 0) {
+      return b;
+    }
+    return is_even(n)
+      ? fast_fib_iter(a, b, square(p) + square(q), square(q) + 2 * p * q, n / 2)
+      : fast_fib_iter(b * q + a * q + a * p, b * p + a * q, p, q, n - 1);
+  }
+  function is_even(x) {
+    return x % 2 == 0;
+  }
+  function square(x) {
+    return x * x;
+  }
+}
 
 console.log(fib_recursion(6));
 console.log(fib_iter(7));
+console.log(fast_fib(6));
